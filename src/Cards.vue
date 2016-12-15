9496229e412911cc67e5210d3c85c6495ca6579d
Augusto Goncalves <augusto.goncalves@autodesk.com>
2016-12-15 17:09:38 -0200

    style adjust

--- src/Cards.vue
+++ src/Cards.vue
@@ -1,13 +1,34 @@
 <template>
     <div id="cards" class="forge-body">
         <div class="forge-top-banner">
-            <h1>Forge is Autodesk's API Platform</h1>
-            <p>The Forge platform unlocks the functionality in Autodesk products that have been serving industries such
-                as architecture, engineering, construction, manufacturing, media, and others.</p>
+            <div  class="maxwidth">
+            <h1>Forge Samples and SDKs</h1>
+            <p>Check out these samples, tutorials and SDKs created to help speed up your Forge application development.
+                Visit our <a href="https://developer.autodesk.com/">blog</a> for lots more smaller code snippets,
+                tips and tricks.</p>
+                </div>
         </div>
         <div class="forge-top-filter">
-            <span style="display:inline-block;">
-                Showing {{repos.length}} repo(s)
+            <div class="maxwidth" style="padding-left:20px; padding-right:20px">
+            <strong>{{repos.length}}</strong> Samples in Github
+            <span style="display:inline-block; float: right; ">
+                Filter by
+                <div class="dropdown" style="display:inline-block;">
+                    <button class="btn btn-info dropdown-toggle btn-xs filterbutton" type="button" data-toggle="dropdown">
+                        API <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li v-for="(repo, api) in apis"><a href="#" @click="filterByAPIUsed(api)">{{ api }}</a></li>
+                    </ul>
+                </div>
+                or
+                <div class="dropdown" style="display:inline-block;">
+                    <button class="btn btn-info dropdown-toggle btn-xs filterbutton" type="button" data-toggle="dropdown">
+                        Language <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li v-for="(repo, language) in languages"><a href="#" @click="filterByLanguage(language)">{{ language }}</a></li>
+                    </ul>
+                </div>
+                or sort by
                 <div class="dropdown" style="display:inline-block;">
                     <button class="btn btn-info dropdown-toggle btn-xs filterbutton" type="button" data-toggle="dropdown">
                         Sort <span class="caret"></span></button>
@@ -17,43 +38,34 @@
                         <li><a href="#" @click="sortByLastUpdated">Last updated</a></li>
                     </ul>
                 </div>
-                <div class="dropdown" style="display:inline-block;">
-                    <button class="btn btn-info dropdown-toggle btn-xs filterbutton" type="button" data-toggle="dropdown">
-                        Language <span class="caret"></span></button>
-                    <ul class="dropdown-menu">
-                        <li v-for="(repo, language) in languages"><a href="#" @click="filterByLanguage(language)">{{ language }}</a></li>
-                    </ul>
-                </div>
-                <div class="dropdown" style="display:inline-block;">
-                    <button class="btn btn-info dropdown-toggle btn-xs filterbutton" type="button" data-toggle="dropdown">
-                        API <span class="caret"></span></button>
-                    <ul class="dropdown-menu">
-                        <li v-for="(repo, api) in apis"><a href="#" @click="filterByAPIUsed(api)">{{ api }}</a></li>
-                    </ul>
-                </div>
             </span>
+                </div>
         </div>
+        <div  class="maxwidth">
         <a v-for="repo in repos" v-bind:href="repo.html_url">
             <div class="repo-card forge-title">
                 <!--<div style="position: absolute; top: 0; right: 0;">
                         <a v-show="repo.homepage" v-bind:href="repo.homepage"><img src="img/demo.png" /></a></div>-->
-                <div class="card-info">
-                  <h3>{{ repo.name }}</h3>
-                  <p>{{ repo.description }}</p>
+                <div>
+                  <h3 class="card-title">{{ repo.name }}</h3>
+                    <p>
+                        <span class="repo-card-info-item"><i class="fa fa-star" aria-hidden="true"></i> {{ repo.stargazers_count }}</span>
+                        <span class="repo-card-info-item"><i class="fa fa-code-fork" aria-hidden="true"></i> {{ repo.forks_count }}</span>
+                        <span class="repo-card-info-item"><i class="fa fa-code" aria-hidden="true"></i> {{ repo.language }}</span>
+                    </p>
+                    <p class="card-info">{{ repo.description }}</p>
                 </div>
                 <div class="card-footer">
                     <!-- This is redundant <span><a v-bind:href="repo.html_url"><i class="fa fa-github" aria-hidden="true"></i> Source Code</a></span>-->
 
                     <!-- need to implement this blogpost feature... not sure how-->
-                    <a v-show="repo.homepage" v-bind:href="repo.homepage" target="_blank"><span
-                            class="repo-card-footer-blogpost"><i class="fa fa-desktop" aria-hidden="true"></i> Live Demo</span></a>
+                    <a v-show="repo.homepage" v-bind:href="repo.homepage" target="_blank" class="repo-card-footer">
+                        <i class="fa fa-desktop" aria-hidden="true"></i> Live Demo</a>
                     <!--<a v-show="repo.blogpost" v-bind:href="repo.blogpost" target="_blank"><span class="repo-card-footer-blogpost"><i class="fa fa-file-text-o" aria-hidden="true"></i> Blog post</span></a>-->
-                    <span class="repo-card-footer-item"><i class="fa fa-star" aria-hidden="true"></i> {{ repo.stargazers_count }}</span>
-                    <span class="repo-card-footer-item"><i class="fa fa-code-fork" aria-hidden="true"></i> {{ repo.forks_count }}</span>
-                    <span class="repo-card-footer-language"><i class="fa fa-code" aria-hidden="true"></i> {{ repo.language }}</span>
                 </div>
             </div>
         </a>
+            </div>
     </div>
 </template>
 
@@ -164,39 +176,55 @@ body {
 
 .filterbutton, .filterbutton:hover, .filterbutton:active, .filterbutton:visited {
   height:25px;
-  background-color: #3566cc !important;
+  background-color: #ffffff !important;
+  color: #333333 !important;
   border: #3566cc !important;
+  height: 33px !important;
+  padding-left: 11px !important;
+  padding-right: 11px !important;
 }
 
 .repo-card {
-  color: #404040;
+  color: #666666;
   background: #fff;
   border: 1px solid #d3d3d3;
   border-radius: 1px;
   display: inline-block;
   height: 20rem;
-  margin: 1rem;
+  margin: 2rem;
   position: relative;
-  width: 300px;
+  width: 345px;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   transition: all 0.3s cubic-bezier(.25,.8,.25,1);
   vertical-align: top;
-}
-.repo-card .card-info {
+  text-align: left;
   padding-left: 1rem;
   padding-right: 1rem;
 }
-.repo-card .card-footer {
-  position: 
-  absolute; 
-  bottom: 10px; 
+
+.card-title{
+  color: #3566cc;
+  padding-bottom: 1rem;
+}
+
+.card-footer {
   width: 100%;
   padding: none;
+  position: absolute;
+  bottom: 10px;
+}
+
+.repo-card-info-item{
+  padding-right: 10px;
+}
+
+.repo-card-footer{
+ color: #EE8822 !important;
 }
 
 .repo-card:hover {
   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  border: 1px solid #3566cc;
 }
-
 
 </style>

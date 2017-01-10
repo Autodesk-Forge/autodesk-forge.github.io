9cc1f4dc32052f71a87f1aef327f605d64fe962c
shiya <luo.shiya@gmail.com>
2017-01-09 19:32:54 -0800

    init
    also added js-search dependency

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import Cards from './Cards.vue';
+import Search from './Search.vue';
 require('bootstrap-loader');
 import './styles/main.less';
 
 new Vue({
   el: '#cards',
   render: h => h(Cards)
+});
+
+new Vue({
+  el: '#search',
+  render: h => h(Search)
 });

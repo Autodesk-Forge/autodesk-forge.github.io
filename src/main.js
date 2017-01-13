1825998125f689190963b6a848d9aa825d5325e2
shiya <luo.shiya@gmail.com>
2017-01-12 18:13:57 -0800

    navbar
    match it to dev portal

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import Cards from './Cards.vue';
 import Search from './Search.vue';
+import Navbar from './Navbar.vue';
 require('bootstrap-loader');
 import './styles/main.less';
+
+new Vue({
+  el: '#navbar',
+  render: h => h(Navbar)
+});
 
 new Vue({
   el: '#cards',
